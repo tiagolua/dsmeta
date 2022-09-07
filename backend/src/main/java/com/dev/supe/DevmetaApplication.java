2d008d721e49314791d74bf8f61e62b3dfaffb2a
Tiago Luã <tiagoluan00@gmailcom>
2022-09-07 16:27:36 -0300

    Project created

---
+++ backend/src/main/java/com/dev/supe/DevmetaApplication.java
@@ -0,0 +1,13 @@
+package com.dev.supe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dev/supe/DevmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.supe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
